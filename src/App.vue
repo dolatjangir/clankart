b721b1ee8d675f0b0cc54263bc9ad901b06a6800
dolatjangir <dolatjangir778@gmail.com>
2025-03-19 23:47:04 +0530

    upload page-2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 // import HelloWorld from './components/HelloWorld.vue'
 // import Home from './components/Home.vue'
-import Page1 from './components/Page1.vue'
+// import Page1 from './components/Page1.vue'
+import Page2 from './components/Page2.vue'
 </script>
 
 <template>
- <Page1/>
+   <Page2/>
+ <!-- <Page1/> -->
  <!-- <Home /> -->
   <!-- <HelloWorld /> -->
 </template>
