b2e179a81cda455980439e78525dc849b2a98e84
dolatjangir <dolatjangir778@gmail.com>
2025-03-21 19:53:47 +0530

    my about-us page

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import Home from './components/Home.vue'
 // import Page1 from './components/Page1.vue'
-import About_us from './components/About_us.vue';
+// import About_us from './components/About_us.vue';
+import Contact_us from './components/Contact_us.vue';
 // import Page2 from './components/Page2.vue'
 </script>
 
 <template>
-  <About_us/>
+  <Contact_us/>
+  <!-- <About_us/> -->
    <!-- <Page2/> -->
  <!-- <Page1/> -->
  <!-- <Home /> -->
